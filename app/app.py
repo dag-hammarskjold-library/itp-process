1d947511338faa66131ba9cc91ed901ed949fec1
jbukhari <joe.bukhari@gmail.com>
2019-11-20 16:44:09 -0500

    merge changes from master

--- app/app.py
+++ app/app.py
@@ -28,7 +28,7 @@ app = Flask(__name__)
 # setting up the secret key and connect to the database
 
 app.secret_key=b'a=pGw%4L1tB{aK6'
-#connect(host=Config.connect_string,db=Config.dbname)
+connect(host=Config.connect_string,db=Config.dbname)
 URL_BY_DEFAULT = 'https://9inpseo1ah.execute-api.us-east-1.amazonaws.com/prod/symbol/'
 
 
@@ -207,17 +207,13 @@ Snapshots are the starting point of an ITP process. Once the ITP producer has
 validated the MARC data for the three relevant bodies and their resepective 
 sessions, they will create a snapshot to begin the remaining part of the 
 process.
-
 The three bodies are A/ E/ and S/
-
 The snapshot function (TBC) performs an Extraction of all the records for the 
 three body/session identifiers and Filters the records to include only the 
 fields the ITP report will need. What is saved in the database is the set of 
 extracted and filtered records necessary to proceed.
-
 From a snapshot, the ITP producer may begin assembling the sections that are 
 part of each body/session document. 
-
 So, in modeling terms: a snapshot has 3 documents; a document has many sections;
 a section uses Rules to Display the values of specific fields, Grouped and 
 Sorted.
@@ -242,7 +238,6 @@ def create_snapshot():
         sessions = request.args.get('sessions').split(',')
         for s in sessions:
             params.append(('191','r',s))
-
         f,s,v = params[0]
         records = Bib.match_value(f,s,v)
         results = []
--- app/config.py
+++ app/config.py
@@ -7,25 +7,19 @@ import os, re
 This is your application configuration file. Use it to set your various configurations,
 but DO NOT put credentials in this file because this file may be checked in to a public
 repository.
-
 Acceptable means of storing/accessing credentials:
   - AWS Parameter Store, e.g.:
-
         import boto3
         client = boto3.client('ssm')
         username = client.get_parameter(Name='username')['Parameter']['Value']
         password = client.get_parameter(Name='password')['Parameter']['Value']
-
     Obviously you need to have the desired parameters already stored in Parameter Store, and you
     should have the awscli installed and configured with a key id and secret key supplied by AWS 
     or your AWS administrator.
-
   - Environment variables:
-
         import os
         username = os.environ.get('username')
         password = os.environ.get('password')
-
 Note that if you use environment variables and plan to incorporate a CI/CD pipeline, e.g., with 
 Travis CI, you will need to make sure your CI environment has the appropriate variables defined
 and stored.
