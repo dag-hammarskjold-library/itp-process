d3d6ff46f7b60b26dbafec34a016a06172dc4172
jbukhari <joe.bukhari@gmail.com>
2019-11-25 11:00:00 -0500

    handle alternate column names

--- app/app.py
+++ app/app.py
@@ -599,7 +599,15 @@ def get_report_by_id(name):
         except:
             raise
             
-        return render_template('report.html',columnTitle=report.field_names,report=report, form=form, resultsSearch=results ,recordNumber=len(results),url=URL_BY_DEFAULT,errorMail=warning)
+        return render_template(
+            'report.html', 
+            columnTitle=report.field_names, 
+            report=report, form=form, 
+            resultsSearch=results, 
+            recordNumber=len(results), 
+            url=URL_BY_DEFAULT, 
+            errorMail=warning
+        )        
     else:
         results = []        
         return render_template('report.html', report=report, form=form)
