88c9a670bd620b6d9e73e201d20ee4fcaae26a9a
Aaron Helton <mariusagricola@gmail.com>
2020-02-28 15:38:52 -0500

    setup aggregations.py

---
+++ app/aggregations.py
@@ -0,0 +1,25 @@
+class Aggregation(object):
+    def __init__(self):
+        self.bodysession = None
+        self.stages = []
+        '''
+        self.match_stage = None
+        self.project_stage = None
+        self.unwind_stage = []
+        self.transform_stage = None
+        self.merge_stage = None
+        '''
+    
+    def exec(self):
+        for stage in self.stages:
+            stage.exec()
+            # Wrap in try ... except and return status.
+
+
+class Stage(object):
+    def __init__(self):
+        self.foo = None
+
+    def exec(self):
+        # do the work
+        pass
