2b6bbcc646016b06632d5cd4ecc4206c5235be5c
Eric Ade ATTERE NGOUA <ericadeatterengoua@Erics-MBP.fios-router.home>
2020-08-20 12:38:49 -0400

    formatting fixed for itpdsl

--- app/word.py
+++ app/word.py
@@ -21,6 +21,7 @@ from flask import send_file, jsonify
 from docx.enum.table import WD_ALIGN_VERTICAL
 from pymongo.collation import Collation
 from docx.enum.table import WD_TABLE_ALIGNMENT
+from docx.enum.section import WD_SECTION
 
 s3_client = boto3.client('s3')
 
@@ -2042,17 +2043,14 @@ def generateWordDocITPDSL(paramTitle,paramSubTitle,bodysession,paramSection,para
 
     # Implementing the "Two columns display" feature
 
-    section = document.sections[0]
-    sectPr = section._sectPr
-    cols = sectPr.xpath('./w:cols')[0]
-    cols.set(qn('w:num'),'2')
 
-    # Marging of the document
 
-    section.top_margin = Cm(2.54)
-    section.bottom_margin = Cm(2.54)
-    section.left_margin = Cm(2.54)
-    section.right_margin = Cm(2.54)
+    # # Marging of the document
+
+    # section.top_margin = Cm(2.54)
+    # section.bottom_margin = Cm(2.54)
+    # section.left_margin = Cm(2.54)
+    # section.right_margin = Cm(2.54)
    
     ################## HEADER ###############################################
     
@@ -2080,7 +2078,7 @@ def generateWordDocITPDSL(paramTitle,paramSubTitle,bodysession,paramSection,para
     
     stlSorentryFont=stlSorentry.font
     stlSorentryFont.name = 'Arial'
-    stlSorentryFont.size = Pt(8)
+    stlSorentryFont.size = Pt(10)
     stlSorentryFont.bold = True
     
     pfSorentry = stlSorentry.paragraph_format
@@ -2126,17 +2124,45 @@ def generateWordDocITPDSL(paramTitle,paramSubTitle,bodysession,paramSection,para
     
     # Adding the Header to the document
     
+    myRecords=setOfData
+    x=0
+
     p=header.add_paragraph(myTitle.upper(), style='New Heading')
     p.alignment = WD_ALIGN_PARAGRAPH.CENTER
     p.add_run("\n")
     p.add_run("\n")
- 
-    myRecords=setOfData
+
+    section = document.sections[0]
+    sectPr = section._sectPr
+    cols = sectPr.xpath('./w:cols')[0]
+    cols.set(qn('w:num'),'1')
+
+    if (bodysession[0]=="A"):
+        p=document.add_paragraph("NOTE :   Languages of corrigenda are indicated only when corrigenda are not issued in all six official languages.Documents issued as Supplements to the Official Records of the General Assembly, Seventy-second Session are so indicated. Information regarding documents bearing the double symbol A/  and S/  can be found in the Supplements to the Official Records of the Security Council. The information provided below is current as of the date this Index is submitted for publication.",style="sornote")
+    
+    if (bodysession[0]=="E"):
+        p=document.add_paragraph("NOTE: Languages of corrigenda are indicated only when corrigenda are not issued in all six languages. Documents issued as Supplements to the Official Records of the Economic and Social Council, 2018 are also indicated. The information provided below is current as of the date this Index is submitted for publication.",style="sornote")
+   
+    if (bodysession[0]=="S"):
+        p=document.add_paragraph("NOTE: Languages of corrigenda are indicated only when corrigenda are not issued in all six official languages. The information provided below is current as of the date this Index is submitted for publication.",style="sornote")
+    
+    p.add_run("\n")
+
+    p=document.add_paragraph(myRecords[0]["committee"],style="sorentry")
+    p.alignment = WD_ALIGN_PARAGRAPH.CENTER
+
+    section = document.add_section(WD_SECTION.CONTINUOUS)
+    sectPr = section._sectPr
+    cols = sectPr.xpath('./w:cols')[0]
+    cols.set(qn('w:num'),'2')
 
     for record in myRecords:
         
-        p1=document.add_paragraph(record["committee"],style="sorentry")       
-        
+        if x>0:
+            p1=document.add_paragraph(record["committee"],style="sorentry")  
+            p1.alignment = WD_ALIGN_PARAGRAPH.CENTER    
+ 
+        x=x+1
         mySeries = record["series"]
         
         for serie in mySeries:
