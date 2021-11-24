d57667241516d7230bcfcf3aeaed21795c2b1cb7
Jennifer <maston@un.org>
2021-11-24 12:24:01 -0500

    itpsubj: Added records for X88 if the code is X27

--- app/aggregations.py
+++ app/aggregations.py
@@ -1696,23 +1696,46 @@ def itpsubj(bodysession):
             }
         }
 
-        #modified
-        add_2['code'] = {
-            '$cond': {
-                'if': {'$isArray': '$191'},
-                'then': {
-                    '$let': {
-                        'vars': {
-                            'a': {'$arrayElemAt': ['$191.b', 0]},
-                            'b': {'$arrayElemAt': ['$191.c', 0]},
-                            'first': {'$trim': {'input': {'$arrayElemAt': ['$191.9', 0]},'chars': ' '}},
-                            'second': {'$trim': {'input': {'$arrayElemAt': ['$191.9', 1]},'chars': ' '}}},
-                        'in': {'$cond': {
-                            'if': {'$and': [
-                                {'$eq': [{'$arrayElemAt': ['$191.b', 0  ]  }, fullbody]}, 
-                                {'$eq': [  {  '$arrayElemAt': [  '$191.c', 0  ]  }, session]}]},
-                            'then': '$$first','else': '$$second'}}}},
-                'else': {'$trim': {'input': '$191.9','chars': ' '}}}
+        # #modified
+        # add_2['code'] = {
+        #     '$cond': {
+        #         'if': {'$isArray': '$191'},
+        #         'then': {
+        #             '$let': {
+        #                 'vars': {
+        #                     'a': {'$arrayElemAt': ['$191.b', 0]},
+        #                     'b': {'$arrayElemAt': ['$191.c', 0]},
+        #                     'first': {'$trim': {'input': {'$arrayElemAt': ['$191.9', 0]},'chars': ' '}},
+        #                     'second': {'$trim': {'input': {'$arrayElemAt': ['$191.9', 1]},'chars': ' '}}},
+        #                 'in': {'$cond': {
+        #                     'if': {'$and': [
+        #                         {'$eq': [{'$arrayElemAt': ['$191.b', 0  ]  }, fullbody]}, 
+        #                         {'$eq': [  {  '$arrayElemAt': [  '$191.c', 0  ]  }, session]}]},
+        #                     'then': '$$first','else': '$$second'}}}},
+        #         'else': {'$trim': {'input': '$191.9','chars': ' '}}}
+        # }
+        add_2['code'] = { 
+            '$cond': { 
+                'if': '$991.f', 
+                'then': '$991.f', 
+                'else': {
+                    '$cond': {
+                        'if': {'$isArray': '$191'},
+                        'then': {
+                            '$let': {
+                                'vars': {
+                                    'a': {'$arrayElemAt': ['$191.b', 0]},
+                                    'b': {'$arrayElemAt': ['$191.c', 0]},
+                                    'first': {'$trim': {'input': {'$arrayElemAt': ['$191.9', 0]},'chars': ' '}},
+                                    'second': {'$trim': {'input': {'$arrayElemAt': ['$191.9', 1]},'chars': ' '}}},
+                                'in': {'$cond': {
+                                    'if': {'$and': [
+                                        {'$eq': [{'$arrayElemAt': ['$191.b', 0  ]  }, fullbody]}, 
+                                        {'$eq': [  {  '$arrayElemAt': [  '$191.c', 0  ]  }, session]}]},
+                                    'then': '$$first','else': '$$second'}}}},
+                        'else': {'$trim': {'input': '$191.9','chars': ' '}}}
+                }
+            }
         }
 
         add_stage2 = {}
@@ -2020,7 +2043,7 @@ def itpsubj(bodysession):
             copyAdd_1['endMM'] = add_1['endMM']
             copyAdd_1['endYY'] = add_1['endYY']
 
-            copyAdd_1['code'] = "X27"
+            copyAdd_1['code'] = '$191.9'
             copyAdd_1['docsymbol'] = add_2['docsymbol']
             copyAdd_1['agendanote'] = add_2['agendanote']
             copyAdd_1['agendanum'] = add_2['agendanum']
@@ -2036,12 +2059,10 @@ def itpsubj(bodysession):
             copyAdd2_stage['$addFields'] = copyAdd_2
 
             copyTransform = {}
+            copyTransform['section'] = transform['section']
             copyTransform['head'] = transform['head']
             copyTransform['subhead'] = transform['subhead']
-            copyTransform['entry'] = {
-                '$concat': ['(', '$votedate', ') ',
-                            { '$substrCP': [ '$agendanote', 62, {'$strLenCP': '$agendanote'} ] },
-                ]}
+            copyTransform['entry'] = {'$concat': ['(', '$votedate', ').']}
             copyTransform['note'] = ''
 
             copyTransform_stage = {}
